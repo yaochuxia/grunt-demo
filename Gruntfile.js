cd885e103d877837e77b65e635fc9fc43a8611cb
姚楚霞 <yaochuxia@hotpu.cn>
2015-04-03 23:48:19 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+// 包装函数
+module.exports = function(grunt) {
+    // 任务配置,所有插件的配置信息
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // uglify插件的配置信息
+        uglify: {
+            options: {
+                banner: '/*! http://JSLite.io - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+                // beautify: true,
+                mangle: true, //不混淆变量名
+                // compress:true,//打开或关闭使用默认选项源压缩。
+            },
+            app_task: {
+                files: {
+                    'build/a.min.js': ['lib/a.js', 'lib/b.js'],
+                    'build/b.min.js': ['lib/a.js']
+                }
+            }
+        },
+        // watch插件的配置信息
+        watch: {
+            another: {
+                files: ['src/*.js','build/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    // Start another live reload server on port 1337
+                    livereload: 1337
+                }
+            }
+        }
+    });
+    // 告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // 告诉grunt当我们在终端中输入grunt时需要做些什么
+    grunt.registerTask('default', ['uglify','watch']);
+ 
+};
